eba6bd314d4ba2c4d6195a6e42913bcd259c6a62
SadmanJawad <jawadnabil977@gmail.com>
2023-06-11 12:52:01 +0600

    manage all classes, tabular form done

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -11,7 +11,7 @@ const Dashboard = () => {
     <div>
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
+        <div className="drawer-content">
           {/* Page content here */}
           <label
             htmlFor="my-drawer-2"
--- src/pages/Dashboard/ManageClass/ManageClass.jsx
+++ src/pages/Dashboard/ManageClass/ManageClass.jsx
@@ -1,8 +1,62 @@
+import useClass from "../../../hooks/useClass";
 
 const ManageClass = () => {
+    const [classes] = useClass();
+
     return (
-        <div>
-        <h1>Manage Class This page will show all the classes</h1>            
+        <div className="w-full h-full">
+        <h1 className="font-caveat text-3xl text-center my-3">Manage All Classes</h1>            
+
+        <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>Class Image & Name</th>
+        <th>Instructor Name</th>
+        <th>Available Seats</th>
+        <th>Price</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+    <tbody>
+{
+    classes.map( cls => <tr key={cls._id}>
+
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src={cls.image} alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">{cls.name}</div>
+            </div>
+          </div>
+        </td>
+        <td>
+        {cls.instructor}
+        </td>
+        <td>{cls.availableSeats}</td>
+        <td>
+            ${cls.price}
+        </td>
+        <td className="flex">
+        <button className="btn  btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-green-600">Approve</button>
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-red-600">Deny</button>
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-sky-300">Feedback</button>
+        </td>
+      </tr> )
+}
+      
+    </tbody>
+   
+    
+  </table>
+</div>
+
+
         </div>
     );
 };
